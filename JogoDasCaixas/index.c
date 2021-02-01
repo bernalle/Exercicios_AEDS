1c03440cb728ddc1614f85cb00c241edfffb8ce0
Gabriel Bernalle <gabrielbbmatozinhos@gmail.com>
2021-02-01 19:26:11 -0300

    introduction

--- JogoDasCaixas/index.c
+++ JogoDasCaixas/index.c
@@ -1,2 +1,20 @@
 #include<stdio.h>
 
+int main(){
+
+    const char nameTxt[10];
+    printf("Digite o nome do arquivo de entrada: ");
+    scanf("%s",&nameTxt);
+
+    char hook; //fisgar caracter dentro do arquivo
+    const int **boxMatrix;
+    FILE *arq = fopen(nameTxt,"rt");
+    if(arq == NULL){
+        printf("Problemas na abertura do arquivo");
+        return 0;
+    }
+    
+
+
+
+}
