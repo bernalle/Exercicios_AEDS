8730be25606dadb6a798920ff13c17152a242b21
Gabriel Bernalle <gabrielbbmatozinhos@gmail.com>
2021-02-01 20:17:46 -0300

    testes arquivo

--- JogoDasCaixas/index.c
+++ JogoDasCaixas/index.c
@@ -1,20 +1,44 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <string.h>
 
-int main(){
+int main()
+{
 
+    printf("\n Entrou");
     const char nameTxt[10];
     printf("Digite o nome do arquivo de entrada: ");
-    scanf("%s",&nameTxt);
+    scanf("%s", &nameTxt);
 
     char hook; //fisgar caracter dentro do arquivo
-    const int **boxMatrix;
-    FILE *arq = fopen(nameTxt,"rt");
-    if(arq == NULL){
+    int **boxMatrix;
+    FILE *arq = fopen(nameTxt, "r");
+    if (arq == NULL)
+    {
         printf("Problemas na abertura do arquivo");
         return 0;
     }
-    
 
+    hook = (fgetc(arq) - 48);
+    boxMatrix[0][0] = hook;
+    int fileira = 1;
+    int caixa = 0;
 
+    while (hook = (fgetc(arq) - 48) != EOF)
+    {
+        for (caixa; caixa != '\n'; caixa++)
+        {
+            boxMatrix[fileira][caixa] = hook;
+        }
+        fileira++;
+    }
+    fclose(arq);
 
+    for (int i = 0; i < 5; i++)
+    {
+        printf("Entrou");
+        for (int j = 0; i < 5; j++)
+        {
+            printf("%i %i",boxMatrix[i][j]);
+        }
+    }
 }
